12451c9cef5d8551412da02efcf83be074af5557
hb <15268745196@163.com>
2025-07-20 11:02:28 +0800

    枚举类

---
+++ src/main/java/com/example/mywork/mapper/FileInfoMapper.java
@@ -0,0 +1,16 @@
+package com.example.mywork.mapper;
+
+import com.example.mywork.entity.FileInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author hb
+ * @since 2025-06-30
+ */
+public interface FileInfoMapper extends BaseMapper<FileInfo> {
+
+}
